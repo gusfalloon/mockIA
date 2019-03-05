cf9be66abafde321c38959ead4ded12051ceca9e
Gus Falloon <augusf@student.cis.edu.hk>
2019-03-05 17:37:10 +0800

    GUI of the search APP first iteration

---
+++ src/ambersearchai/AmberSearchAI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambersearchai;
+
+/**
+ *
+ * @author gusman
+ */
+public class AmberSearchAI {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App searchApp = new App();
+        searchApp.setVisible(true);
+        
+        
+    }
+    
+}
